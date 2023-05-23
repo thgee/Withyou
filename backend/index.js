4dfbe597e5170416002c2f885adbdc262e6fade9
thgee <102576089+thgee@users.noreply.github.com>
2023-05-23 21:04:47 +0900

    FEAT: message 배열들을 백엔드와 연결함
    - 백엔드에서 message 배열을 생성하여, 프론트에서 넘겨준 userMsg와
      assistantMsg를 번갈아가며 push 시도
    
    - 위 과정에서 오류가 발생하여서 아직 미완성 상태임 수정필요

--- backend/index.js
+++ backend/index.js
@@ -19,30 +19,40 @@ app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.post("/interview", async function (req, res) {
+  let { userMsgs, assistantMsgs } = req.body;
+  console.log(assistantMsgs);
+  console.log(userMsgs);
+
+  let messages = [
+    {
+      role: `system`,
+      content: `
+    - 당신은 세계 최고의 면접관입니다.
+    - 당신은 면접자의 능력을 파악할 수 있는 날카로운 질문을 제시해야 합니다.
+    - 질문은 면접자의 지원분야의 수준을 파악할 수 있는 기술질문과, 면접자의 성향을 파악하기 위한 공통질문으로 나뉘어집니다.
+    - 당신은 한번의 발언에 최대 10토큰 끼지만 사용할 수 있습니다.
+    `,
+    },
+    {
+      role: `user`,
+      content: `
+    안녕하세요 면접관님, 저는 지원자 ${req.body.name} 입니다. 제가 지원한 직무는 ${req.body.job} 입니다.`,
+    },
+  ];
+
+  while (userMsgs?.length != 0 && assistantMsgs?.length != 0) {
+    messages.push(assistantMsgs?.pop());
+    messages.push(userMsgs?.pop());
+  }
+
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     temperature: 0.5,
-    max_tokens: 10,
-    messages: [
-      {
-        role: `system`,
-        content: `
-        - 당신은 세계 최고의 면접관입니다.
-        - 당신은 면접자의 능력을 파악할 수 있는 날카로운 질문을 제시해야 합니다.
-        - 질문은 면접자의 지원분야의 수준을 파악할 수 있는 기술질문과, 면접자의 성향을 파악하기 위한 공통질문으로 나뉘어집니다.
-        `,
-      },
-      {
-        role: `user`,
-        content: `
-        안녕하세요 면접관님, 저는 지원자 ${req.body.name} 입니다. 제가 지원한 직무는 ${req.body.job} 입니다.`,
-      },
-    ],
+    max_tokens: 200,
+    messages: messages,
   });
 
   let question = completion.data.choices[0].message[`content`];
-  console.log(question);
-
   res.json({ assistant: question });
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,14 +5,10 @@ function App() {
   const [name, setName] = useState("");
   const [job, setJob] = useState("");
   const [ans, setAns] = useState("");
-  const [res, setRes] = useState("");
   const [userMsgs, setUserMsgs] = useState([]);
   const [assistantMsgs, setAssistantMsgs] = useState([]);
 
-  const handleSubmit = async () => {
-    // userMsg 배열에 현재 유저가 입력한 답변을 추가
-    setUserMsgs([...userMsgs, ans]);
-
+  const handleStartInterview = async () => {
     const response = await fetch(`http://localhost:3000/interview`, {
       method: `POST`,
       headers: {
@@ -21,13 +17,29 @@ function App() {
       body: JSON.stringify({
         name: name,
         job: job,
-        userMsgs: userMsgs,
+      }),
+    });
+  };
+
+  const handleSubmit = async () => {
+    // state는 비동기로 이루어지므로, userMsgs를 직접 사용하면
+    // 업데이트 되기 이전의 값이 api의 body로 들어가게 된다.
+    // 따라서, updatedUserMsgs라는 새로운 배열을 이용하여 처리한다.
+    const updatedUserMsgs = [...userMsgs, ans];
+    setUserMsgs(updatedUserMsgs);
+
+    const response = await fetch(`http://localhost:3000/interview`, {
+      method: `POST`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userMsgs: updatedUserMsgs,
         assistantMsgs: assistantMsgs,
       }),
     });
 
     const data = await response.json();
-    console.log(data);
     await setAssistantMsgs([...assistantMsgs, data.assistant]);
   };
 
@@ -42,15 +54,18 @@ function App() {
         value={name}
         onChange={(e) => setName(e.target.value)}
         onKeyPress={handleEnter}
-        placeholder="이름"
+        placeholder="이름을 입력해주세요"
       />
       <input
         type="text"
         value={job}
         onChange={(e) => setJob(e.target.value)}
         onKeyPress={handleEnter}
-        placeholder="지원 직무"
+        placeholder="지원 직무를 입력해주세요"
       />
+
+      <button onClick={handleStartInterview}> 면접 시작 </button>
+
       <input
         type="text"
         value={ans}
@@ -59,8 +74,9 @@ function App() {
         placeholder="답변"
       />
       <button onClick={handleSubmit}> 전송 </button>
-      {assistantMsgs.map((it) => (
-        <div>{it}</div>
+
+      {assistantMsgs.map((it, idx) => (
+        <div key={idx}>{it}</div>
       ))}
     </div>
   );
