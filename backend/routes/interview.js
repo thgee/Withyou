d0b7d6a4b4b347d0be8f4e5b0eca52ace1ad8abc
thgee <102576089+thgee@users.noreply.github.com>
2023-05-27 00:18:00 +0900

    STYLE: inputAns와 chatBox 사이 간격 조정

--- backend/routes/interview.js
+++ backend/routes/interview.js
@@ -17,27 +17,27 @@ router.use(express.urlencoded({ extended: true })); // for parsing application/x
 router.post("/", async function (req, res) {
   let { messages } = req.body;
   let prompt = [
-    {
-      role: `system`,
-      content: `
-    - 당신은 세계 최고의 면접관입니다.
-    - 당신은 면접자의 능력을 파악할 수 있는 날카로운 질문을 제시해야 합니다.
-    - 질문은 면접자의 지원분야의 수준을 파악할 수 있는 기술질문과, 면접자의 성향을 파악하기 위한 공통질문으로 나뉘어집니다.
-    - 질문은 최대한 간결하게 핵심만 전달하세요
-    `,
-    },
-    {
-      role: `user`,
-      content: `
-      안녕하세요 면접관님, 저는 지원자 [이름] 입니다. 제가 지원한 직무는 [직업] 입니다.`,
-    },
+    // {
+    //   role: `system`,
+    //   content: `
+    // - 당신은 세계 최고의 면접관입니다.
+    // - 당신은 면접자의 능력을 파악할 수 있는 날카로운 질문을 제시해야 합니다.
+    // - 질문은 면접자의 지원분야의 수준을 파악할 수 있는 기술질문과, 면접자의 성향을 파악하기 위한 공통질문으로 나뉘어집니다.
+    // - 질문은 최대한 간결하게 핵심만 전달하세요
+    // `,
+    // },
+    // {
+    //   role: `user`,
+    //   content: `
+    //   안녕하세요 면접관님, 저는 지원자 [이름] 입니다. 제가 지원한 직무는 [직업] 입니다.`,
+    // },
     ...messages,
   ];
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     temperature: 0.5,
-    max_tokens: 50,
+    max_tokens: 2000,
     messages: prompt,
   });
   messages.push(completion.data.choices[0].message);
