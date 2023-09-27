a19932229d861a0e8e44f25f9d1a9088bced6d40
thgee <102576089+thgee@users.noreply.github.com>
2023-09-27 21:33:46 +0900

    FEAT: 프롬프트를 utils 폴더에 분리하였음
    - 면접 시작 시에 면접관의 면접시작 멘트를 화면에 렌더링하도록 해야 함
    - 면접 시작 멘트는 initPrompt가 아닌 message 배열에 추가되도록 해야 함

--- backend/routes/interview.js
+++ backend/routes/interview.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 require("dotenv").config(); // 환경변수 설정
 const { Configuration, OpenAIApi } = require("openai");
+const initPrompt = require("../utils/initPrompt");
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API,
@@ -15,32 +16,17 @@ router.use(express.urlencoded({ extended: true })); // for parsing application/x
 
 router.post("/", async function (req, res) {
   let { messages } = req.body;
-  let prompt = [
-    // {
-    //   role: `system`,
-    //   content: `
-    // - 당신은 세계 최고의 면접관입니다.
-    // - 당신은 면접자의 능력을 파악할 수 있는 날카로운 질문을 제시해야 합니다.
-    // - 질문은 면접자의 지원분야의 수준을 파악할 수 있는 기술질문과, 면접자의 성향을 파악하기 위한 공통질문으로 나뉘어집니다.
-    // - 질문은 최대한 간결하게 핵심만 전달하세요
-    // `,
-    // },
-    // {
-    //   role: `user`,
-    //   content: `
-    //   안녕하세요 면접관님, 저는 지원자 [이름] 입니다. 제가 지원한 직무는 [직업] 입니다.`,
-    // },
-    ...messages,
-  ];
+  let prompt = [...initPrompt, ...messages]; // 초기프롬프트와 대화내역을 프롬프트에 넣어줌
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     temperature: 0.5,
-    max_tokens: 10,
+    max_tokens: 200, // max token : 4097
     messages: prompt,
   });
 
   messages.push(completion.data.choices[0].message);
+  console.log(messages);
   res.status(200).json(messages);
 });
 
---
+++ backend/utils/initPrompt.js
@@ -0,0 +1,16 @@
+const initPrompt = [
+  {
+    role: `system`,
+    content: "",
+  },
+  {
+    role: `user`,
+    content: "면접자 초기 세팅",
+  },
+  {
+    role: `assistant`,
+    content: "면접관 초기 세팅",
+  },
+];
+
+module.exports = initPrompt;
