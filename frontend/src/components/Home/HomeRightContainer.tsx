3903c6f3ecfcfdbece1f18b4470195227afa0b7e
thgee <102576089+thgee@users.noreply.github.com>
2024-03-12 19:16:24 +0900

    FEAT: 면접 시작 버튼 클릭 시 페이지 이동
    - 면접 모드 선택 후 면접 시작 버튼 누르면 해당 면접 페이지로 이동
    - 현재는 실전면접만 연결되어 있음

--- frontend/src/components/Home/HomeRightContainer.tsx
+++ frontend/src/components/Home/HomeRightContainer.tsx
@@ -1,12 +1,33 @@
 import styles from "../../styles/componentStyles/HomeRightContainer.module.scss";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { HomeRightContainerProps } from "../../types/types";
 import { Transition } from "react-transition-group";
+import { useNavigate } from "react-router-dom";
 
 const HomeRightContainer: FC<HomeRightContainerProps> = ({
   selectedMode,
   rightContainerRef,
 }) => {
+  const [name, setName] = useState<string>("");
+  const [job, setJob] = useState<string>("");
+  const navigator = useNavigate();
+
+  const handleInterviewStart = () => {
+    switch (selectedMode) {
+      case 0:
+        break;
+      case 1:
+        navigator("/actual");
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <Transition in={selectedMode !== -1} timeout={500}>
       {(state: any) => (
@@ -18,10 +39,24 @@ const HomeRightContainer: FC<HomeRightContainerProps> = ({
 
           <div className={styles.input_user_info_box}>
             <h4>이름</h4>
-            <input type="text" placeholder="이름을 입력하세요" />
+            <input
+              type="text"
+              placeholder="이름을 입력하세요"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
             <h4>직무</h4>
-            <input type="text" placeholder="직무를 입력하세요" />
-            <button>면접 시작</button>
+            <input
+              type="text"
+              placeholder="직무를 입력하세요"
+              value={job}
+              onChange={(e) => {
+                setJob(e.target.value);
+              }}
+            />
+            <button onClick={handleInterviewStart}>면접 시작</button>
           </div>
         </div>
       )}
